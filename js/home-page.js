06278a7e094b074366f2fb5b6982b065ef8e8209
ArseneBrosy <arsene.brosy@divtec.ch>
2025-01-10 10:20:59 +0100

    list boulders

--- js/home-page.js
+++ js/home-page.js
@@ -1,3 +1,15 @@
+const bouldersList = document.querySelector('#boulders');
+
 function listBoulders(boulders) {
-  console.log(boulders);
+  bouldersList.innerHTML = '';
+  for (const boulder of boulders) {
+    bouldersList.innerHTML += `<a href="boulder.html">
+        <img src="images/cotations/${boulder.grade}.png">
+        <div>
+            <p class="title">${boulder.name}</p>
+            <p class="routesetter">${boulder.setter}</p>
+        </div>
+        <p class="date">${boulder.date}</p>
+    </a>`;
+  }
 }
