a71a7559dc4829319e9ebd7d5eeba3caae839537
ArseneBrosy <arsene.brosy@divtec.ch>
2025-01-16 10:00:38 +0100

    detect hold

--- js/wall.js
+++ js/wall.js
@@ -22,6 +22,59 @@ function drawWall(boulder, zoom = 1, x = 0, y = 0, otherHolds = true) {
     holdSprite.src = `./images/wall/holds/${hold}.png`;
     holdSprite.onload = () => {
       ctx.drawImage(holdSprite, x, y, canvas.width * zoom, canvas.height * zoom);
+
+      // start
+      if (boulder[hold] === 1) {
+        const position = getHoldPosition(holdSprite);
+        console.log(position);
+        ctx.strokeRect(position.left, position.top,
+          position.right - position.left, position.bottom - position.top);
+      }
     }
+  }
+}
+
+function getHoldPosition(holdSprite) {
+  // Créer un canvas temporaire
+  const _canvas = document.createElement('canvas');
+  const _ctx = _canvas.getContext('2d');
+
+  _canvas.width = holdSprite.width;
+  _canvas.height = holdSprite.height;
+
+  // Dessiner l'image sur le canvas
+  _ctx.drawImage(holdSprite, 0, 0);
+
+  // Obtenir les données des pixels
+  const imageData = _ctx.getImageData(0, 0, _canvas.width, _canvas.height);
+  const pixels = imageData.data;
+
+  // Initialiser les limites
+  let top = holdSprite.height;
+  let bottom = 0;
+  let left = holdSprite.width;
+  let right = 0;
+
+  // Parcourir tous les pixels
+  for (let y = 0; y < holdSprite.height; y++) {
+    for (let x = 0; x < holdSprite.width; x++) {
+      const index = (y * holdSprite.width + x) * 4; // Index du pixel dans le tableau
+      const alpha = pixels[index + 3]; // Canal alpha
+
+      if (alpha > 1) {
+        ctx.fillRect(x, y, 0, 0);
+
+        top = Math.min(top, y);
+        bottom = Math.max(bottom, y);
+        left = Math.min(left, x);
+        right = Math.max(right, x);
+      }
+    }
+  }
+
+  if (top <= bottom && left <= right) {
+    return { top, bottom, left, right };
+  } else {
+    return null;
   }
 }
