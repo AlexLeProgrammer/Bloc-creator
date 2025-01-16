6a83d438c45efc3cf599ccb4e6153d66ff981301
ArseneBrosy <arsene.brosy@divtec.ch>
2025-01-16 09:16:25 +0100

    get holds from db

--- js/boulder.js
+++ js/boulder.js
@@ -16,18 +16,5 @@ function fillValues(boulder) {
 
   document.querySelector('title').innerText = boulder.name;
 
-  drawWall({
-    "g31": 2,
-    "j30": 0,
-    "f22": 0,
-    "l22": 0,
-    "h18": 0,
-    "h12": 0,
-    "o15": 0,
-    "v11": 0,
-    "r6": 0,
-    "l5": 0,
-    "p1": 1,
-    "s1": 1,
-  });
+  drawWall(boulder.holds);
 }
